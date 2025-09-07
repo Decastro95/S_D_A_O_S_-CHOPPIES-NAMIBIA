8f76aeb78aa65d1ef52f569620a897835d1e6a93
Gustavo JrNdatipo <113350810+Decastro95@users.noreply.github.com>
2025-09-07 20:19:41 +0000

    Update dashboards

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link rel="icon" href="/logo-choppies.png" />
+        <meta name="theme-color" content="#D90429" />
+      </Head>
+      <body className="bg-gray-100">
         <Main />
         <NextScript />
       </body>
